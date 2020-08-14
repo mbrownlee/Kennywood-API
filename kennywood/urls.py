8f49abebf9a54d94da643015641f3882edd64a07
Joe Shepherd <joeshepmedia@gmail.com>
2020-08-14 11:33:11 -0500

    update urls config for 3.x

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from kennywoodapi.models import *
@@ -14,9 +14,9 @@ router.register(r'itineraryitems', ItineraryView, 'itinerary')
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^register$', register_user),
-    url(r'^login$', login_user),
-    url(r'^api-token-auth$', obtain_auth_token),
-    url(r'^api-auth$', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
+    path('register/', register_user),
+    path('login/', login_user),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
